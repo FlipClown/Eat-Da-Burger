b1a93f6ef16422648cd0f120b718a62c030fd21d
FlipClown <sourmento@gmail.com>
2017-12-11 18:18:08 -0500

    Add files via upload

---
+++ Eat-Da-Burger/db/seeds.sql
@@ -0,0 +1,14 @@
+USE burgers_db;
+
+INSERT INTO burgers
+    (burger_name, devoured)
+VALUES
+    ('amazing burger', false);
+INSERT INTO burgers
+    (burger_name, devoured)
+VALUES
+    ('alright burger', false);
+INSERT INTO burgers
+    (burger_name, devoured)
+VALUES
+    ('bad burger', false);
